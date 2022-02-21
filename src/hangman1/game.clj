89883801b272372f4db1f092b304cfe0e13212a4
morelight101 <supernikita@gmail.com>
2022-02-21 14:02:43 +0100

    new-game becomes pure

--- src/hangman1/game.clj
+++ src/hangman1/game.clj
@@ -1,15 +1,6 @@
 (ns game
   (:require [clojure.string :refer [split]]))
 
-;; @Nico: Das Repro ist ein bisschen unaufgeräumt.
-
-  ;; {:global
-  ;;  {:word-population ["Palm" "Tree" "guide"]
-  ;;   :allowed-letters "abcdefghijklmnopqrstuvwxyzäöüßABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ"
-  ;;   :Points-value 8
-  ;;   :max-guesses 5}
-
-
 (defn game-won?
   "Liefert true, wenn der Spieler das Spiel `game `gewonnen hat, andernfalls false. "
   [{:keys [word-to-be-guessed correct-guesses]} ]
@@ -29,18 +20,33 @@
 
 
 ;; @Nico: Die Funktion ist nicht pure. Du möchtest als Programmierer, der diese Schicht nutzt, ein neues Spiel anlegen können. Wie kann das pure gehen?
+;; @Ingo
+;; pure bedeutet: no side-effects + same input -> same output
+;; Irgendwo muss aber das word-to-guess ändern;
+;; Vorschlag 1: insert-rand-word! wird nach new-game aufgerufen
+;; Vorschlag 2: word-to-guess aus dem game auslagern
+;; Warum Vorschlag 1 ?
+;; Daten die zusammengehören, sollen zusammenbleiben zwecks Verständlichkeit für den Programmierer.
+;; Pure functions sollen gleichfalls das Verständnis erleichtern, deshalb
+;; dezidierter (= easily testable) Zustand zu Beginn, der anschliessend
+;; impure wird. 
 
-(defn new-game!
+(defn new-game
   "creates a new game map"
   []
   {
-   :word-to-guess (nth ["Palm" "bank" "Tree"](rand-int 3)) 
+   :word-to-guess "Test"
    :correct-guesses '#{}
    :tries-left 5
    }
   )
 
+(defn insert-rand-word!
+  ""
+  [game]
+  (assoc-in game  [:word-to-guess] (nth ["Palm" "Tree" "Vacation"] (rand-int 3)))
 
+  )
 
 
 
