c39103afe6804d204c7d15fb01d5d4cb4c8644a8
morelight101 <supernikita@gmail.com>
2022-02-21 14:21:19 +0100

    fixed docstring

--- src/hangman1/game.clj
+++ src/hangman1/game.clj
@@ -3,21 +3,18 @@
 
 (defn game-won?
   "Liefert true, wenn der Spieler das Spiel `game `gewonnen hat, andernfalls false. "
-  [{:keys [word-to-be-guessed correct-guesses]} ]
+  [{:keys [word-to-be-guessed correct-guesses]}]
   (= (into #{} word-to-be-guessed) correct-guesses))
-
 
 (defn game-lost?
   "returns true if player lost, otherwise false"
   [{:keys [tries-left]}]
   (zero? tries-left))
 
-
 (defn game-over?
   "returns true if user lost or won the game, otherwise false"
   [game]
   (or (game-won? game) (game-lost? game)))
-
 
 ;; @Nico: Die Funktion ist nicht pure. Du möchtest als Programmierer, der diese Schicht nutzt, ein neues Spiel anlegen können. Wie kann das pure gehen?
 ;; @Ingo
@@ -34,19 +31,14 @@
 (defn new-game
   "creates a new game map"
   []
-  {
-   :word-to-guess "Test"
+  {:word-to-guess "Test"
    :correct-guesses '#{}
-   :tries-left 5
-   }
-  )
+   :tries-left 5})
 
 (defn insert-rand-word!
-  ""
+  "selects a random word and makes it available to the game"
   [game]
-  (assoc-in game  [:word-to-guess] (nth ["Palm" "Tree" "Vacation"] (rand-int 3)))
-
-  )
+  (assoc-in game  [:word-to-guess] (nth ["Palm" "Tree" "Vacation"] (rand-int 3))))
 
 
 
--- test/hangman1/game.clj
+++ test/hangman1/game.clj
@@ -4,54 +4,40 @@
 
 ;ein verlorenes Spiel (der Spieler konnte das Wort nicht erraten)
 (def game-lost
- { :word-to-be-guessed ""
+  {:word-to-be-guessed ""
    :correct-guesses 0
-   :tries-left 0}
-  )
+   :tries-left 0})
 
 ;;ein gewonnenes Spiel (der Spieler konnte das Wort erraten)
 (def game-won
- { :word-to-be-guessed ""
+  {:word-to-be-guessed ""
    :correct-guesses 0
-   :tries-left 0}
-  )
+   :tries-left 0})
 
 ;;ein laufendes Spiel
 (def game-ongoing
- { :word-to-be-guessed ""
+  {:word-to-be-guessed ""
    :correct-guesses 0
-   :tries-left 0}
-  )
-
+   :tries-left 0})
 
 (deftest f-game-won
   (testing "game-lost?"
-    (is true  (game-won? game-won ))
-    (is false (game-won? game-lost ))
+    (is true  (game-won? game-won))
+    (is false (game-won? game-lost))
     (is false (game-won? game-ongoing))))
-
-
-
 
 (deftest f-game-lost
   (testing "game-lost?"
-    (is false (game-lost? game-won ))
-    (is  true  (game-lost? game-lost ))
-    (is  false (game-lost? game-ongoing ))
-    )
-)
+    (is false (game-lost? game-won))
+    (is  true  (game-lost? game-lost))
+    (is  false (game-lost? game-ongoing))))
 
 (deftest f-game-over
   (testing "game-over?")
-    (is  false (game-lost? game-won ))
-    (is  true  (game-lost? game-lost ))
-    (is  false (game-lost? game-ongoing))
-  )
-
+  (is  false (game-lost? game-won))
+  (is  true  (game-lost? game-lost))
+  (is  false (game-lost? game-ongoing)))
 
 (deftest f-new-game
   (testing "new-game"
-    (is = ((new-game! gamedata) gamedata)))
-  )
-
-
+    (is = ((new-game! gamedata) gamedata))))
