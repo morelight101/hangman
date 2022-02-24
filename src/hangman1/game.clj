409d2eda03c7390ed6eaf38fca9713e2b3adf3b2
morelight101 <supernikita@gmail.com>
2022-02-24 22:44:24 +0100

    style `, ->

--- src/hangman1/game.clj
+++ src/hangman1/game.clj
@@ -1,4 +1,4 @@
-(ns game
+(ns  hangman1.game
   (:require [clojure.string :refer [includes?]]))
 
 (defn word->letters
@@ -7,10 +7,10 @@
   (mapv str word))
 
 (defn game-won?
-  "Liefert true, wenn der Spieler das Spiel `game `gewonnen hat,
+  "Liefert true, wenn der Spieler das Spiel `game` gewonnen hat,
   andernfalls false. "
   [{:keys [word-to-guess correct-guesses]}]
-  (= (into (set "") (word->letters word-to-guess)) correct-guesses))
+  (= (into #{} (word->letters word-to-guess)) correct-guesses))
 
 (defn game-lost?
   "Returns true if player lost, otherwise false."
@@ -18,46 +18,43 @@
   (zero? tries-left))
 
 (defn game-over?
-  "Returns true if user guessed the word or has no tries left,
-  otherwise false."
+  "Returns true if user guessed the word or has no `tries-left`,
+  otherwise false.See also [[game-won?]] and [[game-lost?]]"
   [game]
   (or (game-won? game) (game-lost? game)))
 
 (defn new-game
-  "creates a new game, parameter is a string"
+  "creates a new `game`, parameter is a string"
   [word-to-guess]
   {:word-to-guess  word-to-guess
-   :correct-guesses '(set "")
+   :correct-guesses '#{}
    :tries-left 5})
 
 (defn guess-letter
-  "Returns an updated game reflecting whether the letter the user
-  guessed was contained in the word-to-guess;
-  is the game over, the function returns the game without change."
+  "Returns an updated `game` reflecting whether the `letter`
+  the user guessed was contained in the `word-to-guess`;
+  is the `game` over, the function returns the `game`
+  without change."
   [{:keys [word-to-guess] :as game} letter]
   (cond
     (game-over? game) game
     (includes? word-to-guess letter)
-    (update-in game [:correct-guesses] conj letter)
-    :else (update-in game [:tries-left] dec)))
+    (update game :correct-guesses (conj letter))
+    :else (update game :tries-left dec)))
 
 (defn score
-  "Returns the current score."
+  "Returns the current `score`."
   [{:keys [correct-guesses]}]
   (-> correct-guesses
       (count)
       (* 8)))
 
 (defn hint
-  "Returns a vector of correctly guessed letters in correct position;
-  not yet correctly guessed letters are indicated by nil."
+  "Returns a vector of `correct-guesses` in correct position;
+  incorrectly guessed letters are indicated by nil."
   [{:keys [word-to-guess correct-guesses]}]
   (->>
       word-to-guess
       (word->letters)
       (mapv correct-guesses)
-   ))
-
-
-
-
+      ))
--- test/hangman1/game_test.clj
+++ test/hangman1/game_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest is testing]]
             [game :refer :all]))
 
+
 ;ein verlorenes Spiel (der Spieler konnte das Wort nicht erraten)
 (def game-lost
   {:word-to-guess "tree"
@@ -12,7 +13,7 @@
 (def game-won
   {:word-to-guess "tree"
    :correct-guesses #{"t" "r" "e"}
-   :tries-left 0})
+   :tries-left 2})
 
 ;;ein laufendes Spiel
 (def game-ongoing
@@ -20,46 +21,49 @@
    :correct-guesses #{"t"}
    :tries-left 2})
 
-;;ein ganz neues Spiel
+;;ein neues Spiel
 (def game-new
   {:word-to-guess "tree"
    :correct-guesses #{}
    :tries-left 5})
 
 (deftest f-word->letters
-  (testing "word->letters"
+  (testing "`word->letters`"
     (is (= ["t" "r" "e" "e"]
            (word->letters "tree")))))
 
 (deftest f-game-won
-  (testing "game-lost?"
+  (testing "`game-won`"
     (is (= true  (game-won? game-won)))
     (is (= false (game-won? game-lost)))
     (is (= false (game-won? game-new)))
     (is (= false (game-won? game-ongoing)))))
 
 (deftest f-game-lost
-  (testing "game-lost?"
+  (testing "`game-lost?`"
     (is (= false (game-lost? game-won)))
     (is (= false (game-lost? game-new)))
     (is (= true  (game-lost? game-lost)))
     (is (= false (game-lost? game-ongoing)))))
 
 (deftest f-game-over
-  (testing "game-over?"
-    (is (= false (game-lost? game-won)))
-    (is (= true  (game-lost? game-lost)))
-    (is (= false  (game-lost? game-new)))
-    (is (= false (game-lost? game-ongoing)))))
+  (testing "`game-over?`"
+    (is (= false (game-over? game-won)))
+    (is (= true  (game-over? game-lost)))
+    (is (= false (game-over? game-new)))
+    (is (= false (game-over? game-ongoing)))))
 
 (deftest f-new-game
-  (testing "does new-game 'tree' return the proper map"
+  (testing "does `new-game` 'tree' return the proper map"
     (is = ((new-game "tree") game-new))))
 
 (deftest f-hint
-  (testing "will hint return correct vector for word-to-guess tree
-            and :correct-guesses #{'t'}"
+  (testing "will `hint` return correct vector for `word-to-guess` `tree`
+            and `:correct-guesses` `#{'t'}` "
     (is (= ["t" nil nil nil] (hint game-ongoing) ))))
 
 
-
+(deftest f-score
+  (testing "does `score` compute correctly"
+    (is (= 0  (score (new-game "tree"))))
+    (is (= 24 (score game-won)))))
